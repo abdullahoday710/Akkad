a751b3089e74ed73bc64fb9843a0839370652449
vertexpotato <abdallaoday7@gmail.com>
2021-06-16 19:39:42 +0300

    moved warning to IsValid()

--- Akkad/src/Akkad/Graphics/Sprite.cpp
+++ Akkad/src/Akkad/Graphics/Sprite.cpp
@@ -35,12 +35,12 @@ namespace Akkad {
 
 		bool Sprite::IsUsingTilemap()
 		{
-			if (m_Material->HasTexture("main_sprite_tex"))
+			if (IsValid())
 			{
 				return m_Material->GetTexture("main_sprite_tex")->GetDescriptor().IsTilemap;
 			}
-			
-			AK_WARNING("Sprite material must have a porperty named 'main_sprite_tex' for the main sprite texture !");
+
+			return false;
 		}
 
 		bool Sprite::IsValid()
@@ -49,7 +49,11 @@ namespace Akkad {
 			{
 				if (m_Material->isValid())
 				{
-					return true;
+					if (m_Material->HasTexture("main_sprite_tex"))
+					{
+						return true;
+					}
+					AK_WARNING("Sprite material must have a porperty named 'main_sprite_tex' for the main sprite texture !");
 				}
 			}
 
@@ -64,22 +68,20 @@ namespace Akkad {
 			}
 			else
 			{
-				if (!m_Material->HasTexture("main_sprite_tex"))
+				if (IsValid())
 				{
-					AK_WARNING("Sprite material must have a porperty named 'main_sprite_tex' for the main sprite texture !");
-					return;
+					auto desc = m_Material->GetTexture("main_sprite_tex")->GetDescriptor();
+					float atlasWidth = desc.Width;
+					float atlasHeight = desc.Height;
+
+					// coords of the bottom left
+					m_MinTextureCoords.x = (m_TileRow * desc.TileWidth) / atlasWidth;
+					m_MinTextureCoords.y = (m_TileColoumn * desc.TileHeight) / atlasHeight;
+
+					// coords of the top right
+					m_MaxTextureCoords.x = ((m_TileRow + 1) * desc.TileWidth) / atlasWidth;
+					m_MaxTextureCoords.y = ((m_TileColoumn + 1) * desc.TileHeight) / atlasHeight;
 				}
-				auto desc = m_Material->GetTexture("main_sprite_tex")->GetDescriptor();
-				float atlasWidth = desc.Width;
-				float atlasHeight = desc.Height;
-
-				// coords of the bottom left
-				m_MinTextureCoords.x = (m_TileRow * desc.TileWidth) / atlasWidth;
-				m_MinTextureCoords.y = (m_TileColoumn * desc.TileHeight) / atlasHeight;
-
-				// coords of the top right
-				m_MaxTextureCoords.x = ((m_TileRow + 1) * desc.TileWidth) / atlasWidth;
-				m_MaxTextureCoords.y = ((m_TileColoumn + 1) * desc.TileHeight) / atlasHeight;
 			}
 		}
 	}
--- Editor/src/Editor/Panels/SpriteSelectorPanel.cpp
+++ Editor/src/Editor/Panels/SpriteSelectorPanel.cpp
@@ -27,69 +27,65 @@ namespace Akkad {
 			if (m_ActiveEntity.IsValid())
 			{
 				auto& sprite = m_ActiveEntity.GetComponent<SpriteRendererComponent>();
-				if (sprite.sprite.GetMaterial() != nullptr)
+				if (sprite.sprite.IsValid())
 				{
-					if (sprite.sprite.GetMaterial()->isValid())
+					glm::vec2 tilemapPreviewSize = { 400,400 };
+					auto tilemap = sprite.sprite.GetMaterial()->GetTexture("main_sprite_tex");
+					ImGui::Image((void*)tilemap->GetID(), { tilemapPreviewSize.x, tilemapPreviewSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
+
+					auto rectmin = ImGui::GetItemRectMin();
+					auto rectmax = ImGui::GetItemRectMax();
+					auto drawlist = ImGui::GetForegroundDrawList();
+					drawlist->AddRect(rectmin, rectmax, IM_COL32(255, 255, 0, 255));
+
+					unsigned int total_rows = tilemap->GetDescriptor().Width / tilemap->GetDescriptor().TileWidth;
+					unsigned int total_cols = tilemap->GetDescriptor().Height / tilemap->GetDescriptor().TileHeight;
+
+					float originalTileWidth = tilemap->GetDescriptor().TileWidth;
+					float originalTileHeight = tilemap->GetDescriptor().TileHeight;
+
+					glm::vec2 TileMapScale = { tilemapPreviewSize.x / tilemap->GetDescriptor().Width,  tilemapPreviewSize.y / tilemap->GetDescriptor().Height };
+
+					float scaledTileWidth = originalTileWidth * TileMapScale.x;
+					float scaledTileHeight = originalTileHeight * TileMapScale.y;
+
+					ImVec2 originalTileMin = { rectmin.x, rectmax.y };
+					ImVec2 originalTileMax = { rectmin.x + scaledTileWidth, rectmax.y - scaledTileHeight };
+
+					ImVec2 currentTileMin = originalTileMin;
+					ImVec2 currentTileMax = originalTileMax;
+					for (size_t current_col = 0; current_col < total_cols; current_col++)
 					{
-						glm::vec2 tilemapPreviewSize = { 400,400 };
-						auto tilemap = sprite.sprite.GetMaterial()->GetTexture("sprite");
-						ImGui::Image((void*)tilemap->GetID(), { tilemapPreviewSize.x, tilemapPreviewSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
-
-						auto rectmin = ImGui::GetItemRectMin();
-						auto rectmax = ImGui::GetItemRectMax();
-						auto drawlist = ImGui::GetForegroundDrawList();
-						drawlist->AddRect(rectmin, rectmax, IM_COL32(255, 255, 0, 255));
-
-						unsigned int total_rows = tilemap->GetDescriptor().Width / tilemap->GetDescriptor().TileWidth;
-						unsigned int total_cols = tilemap->GetDescriptor().Height / tilemap->GetDescriptor().TileHeight;
-
-						float originalTileWidth = tilemap->GetDescriptor().TileWidth;
-						float originalTileHeight = tilemap->GetDescriptor().TileHeight;
-
-						glm::vec2 TileMapScale = { tilemapPreviewSize.x / tilemap->GetDescriptor().Width,  tilemapPreviewSize.y / tilemap->GetDescriptor().Height };
-
-						float scaledTileWidth = originalTileWidth * TileMapScale.x;
-						float scaledTileHeight = originalTileHeight * TileMapScale.y;
-
-						ImVec2 originalTileMin = { rectmin.x, rectmax.y };
-						ImVec2 originalTileMax = { rectmin.x + scaledTileWidth, rectmax.y - scaledTileHeight };
-
-						ImVec2 currentTileMin = originalTileMin;
-						ImVec2 currentTileMax = originalTileMax;
-						for (size_t current_col = 0; current_col < total_cols; current_col++)
+						if (current_col != 0)
 						{
-							if (current_col != 0)
-							{
-								currentTileMin.y -= scaledTileHeight;
-								currentTileMax.y -= scaledTileHeight;
-							}
-
-							currentTileMin.x = originalTileMin.x;
-							currentTileMax.x = originalTileMax.x;
-							for (size_t current_row = 0; current_row < total_rows; current_row++)
-							{
-								drawlist->AddRect(currentTileMin, currentTileMax, IM_COL32(255, 255, 0, 255));
-								auto input = Application::GetInputManager();
-
-								if (input->GetMouseDown(MouseButtons::LEFT))
-								{
-									int mouseX = input->GetMouseX();
-									int mouseY = input->GetMouseY();
-
-									if (mouseX < currentTileMax.x && mouseY > currentTileMax.y)
-									{
-										if (mouseX > currentTileMin.x && mouseY < currentTileMin.y)
-										{
-											sprite.sprite.SetTileRow((float)current_row);
-											sprite.sprite.SetTileColoumn((float)current_col);
-										}
-									}
-								}
-								currentTileMin.x += scaledTileWidth;
-								currentTileMax.x += scaledTileWidth;
-							}
+							currentTileMin.y -= scaledTileHeight;
+							currentTileMax.y -= scaledTileHeight;
 						}
 
+						currentTileMin.x = originalTileMin.x;
+						currentTileMax.x = originalTileMax.x;
+						for (size_t current_row = 0; current_row < total_rows; current_row++)
+						{
+							drawlist->AddRect(currentTileMin, currentTileMax, IM_COL32(255, 255, 0, 255));
+							auto input = Application::GetInputManager();
+
+							if (input->GetMouseDown(MouseButtons::LEFT))
+							{
+								int mouseX = input->GetMouseX();
+								int mouseY = input->GetMouseY();
+
+								if (mouseX < currentTileMax.x && mouseY > currentTileMax.y)
+								{
+									if (mouseX > currentTileMin.x && mouseY < currentTileMin.y)
+									{
+										sprite.sprite.SetTileRow((float)current_row);
+										sprite.sprite.SetTileColoumn((float)current_col);
+									}
+								}
+							}
+							currentTileMin.x += scaledTileWidth;
+							currentTileMax.x += scaledTileWidth;
+						}
 					}
 				}
 			}

9ca72f56412d252e2d29dde55a8722fbcbe0d57b
vertexpotato <abdallaoday7@hotmail.com>
2022-04-23 00:13:26 +0300

    fixed document not found bug when using pthreads in web platform

--- Runtime/WebModules/include_assets.js
+++ Runtime/WebModules/include_assets.js
@@ -1,10 +1,12 @@
-function includeJs(jsFilePath) {
-    var js = document.createElement("script");
-
-    js.type = "text/javascript";
-    js.src = jsFilePath;
-
-    document.body.appendChild(js);
+if(typeof document != "undefined")
+{
+    function includeJs(jsFilePath) 
+    {
+        var js = document.createElement("script");
+        js.type = "text/javascript";
+        js.src = jsFilePath;
+        document.body.appendChild(js);
+    }
+       
+       includeJs("assets_preload.js");
 }
-
-includeJs("assets_preload.js");
